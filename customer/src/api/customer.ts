7c8c243ad8c7c457f2b706277552d7258b0de286
satishp21 <satishdpanchal786@gmail.com>
2024-05-09 14:35:37 +0530

    custumer service file structure modified

--- customer/src/api/customer.ts
+++ customer/src/api/customer.ts
@@ -1,85 +1,22 @@
 import { Request, Response, NextFunction, Application } from 'express';
 import CustomerService from '../services/customer-service';
 import { middleware } from './middlewares/auth';
-import { PublishMessage, getCaching, setCaching } from '../utils';
+import { AddAddress, deleteProfile, getProfile, login, signUp } from '../controllers/customer';
 
 export const customerRoute = (app: Application, channel: any, redisClient: any): void => {
-  const service: CustomerService = new CustomerService();
-
-  app.post('/signup', async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const { email, password, phone }: { email: string; password: string; phone: string } = req.body;
-      const data = await service.SignUp({ email, password, phone });
-      return res.json(data);
-    } catch (error) {
-      next(error);
-    }
+  app.use((req, res, next) => {
+    res.locals.redisClient = redisClient; // Pass the Redis client
+    res.locals.channel = channel; // Pass the channel
+    next();
   });
 
-  app.post('/login', async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const { email, password }: { email: string; password: string } = req.body;
-      const data = await service.SignIn({ email, password });
-      return res.json(data);
-    } catch (error) {
-      next(error);
-    }
-  });
+  app.post('/signup', signUp);
 
-  app.post('/address', middleware, async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const { _id }: { _id: string } = res.locals.user;
-      const {
-        street,
-        postalCode,
-        city,
-        country,
-      }: { street: string; postalCode: string; city: string; country: string } = req.body;
-      const data = await service.AddNewAddress(_id, {
-        street,
-        postalCode,
-        city,
-        country,
-      });
-      await setCaching(redisClient, `Profile${_id}`, data);
-      return res.json(data);
-    } catch (error) {
-      next(error);
-    }
-  });
+  app.post('/login', login);
 
-  app.get('/profile', middleware, async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const { _id }: { _id: string } = res.locals.user;
-      let { isCached, results }: { isCached: boolean; results: any } = await getCaching(redisClient, `Profile${_id}`);
-      if (results) {
-        return res.json({ data: results, fromCache: isCached });
-      } else {
-        results = await service.GetProfile(_id);
-        if (results.length === 0) {
-          throw new Error('Api returned with an empty array');
-        }
-        await setCaching(redisClient, `Profile${_id}`, results);
-        return res.json({ data: results, fromCache: isCached });
-      }
-    } catch (error) {
-      next(error);
-    }
-  });
+  app.post('/address', middleware, AddAddress);
 
-  app.delete('/profile', middleware, async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const { _id }: { _id: string } = res.locals.user;
-      const { data, payload }: { data: any; payload: any } = await service.DeleteProfile(_id);
-      // Send message to Shopping Service for removing cart & wishlist
-      PublishMessage(channel, 'shopping__service', JSON.stringify(payload));
-      return res.json(data);
-    } catch (error) {
-      next(error);
-    }
-  });
+  app.get('/profile', middleware, getProfile);
 
-  app.get('/whoami', (req: Request, res: Response, next: NextFunction) => {
-    return res.status(200).json({ msg: '/customer : I am Customer Service' });
-  });
+  app.delete('/profile', middleware, deleteProfile);
 };
